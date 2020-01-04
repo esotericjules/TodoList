01af3f240294f74f89028c9d667663af8e5a2673
juliet.onyekaoha <juliet.onyekaoha@venturegardengroup.com>
2020-01-04 13:24:42 +0100

    implemented checkbox cancel and delete todo functionalities

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const taskInput = document.querySelector('#task');
 
 //DOM elements for new implementation
 const documentFragment = document.createDocumentFragment();
-const card = document.querySelector(".card")
+const checkboxRow = document.querySelector(".checkbox-row")
 const addNew = document.querySelector(".add-new");
 const date = document.querySelector(".datepicker");
 const time= document.querySelector('.timepicker');
-
+const checkbox = document.querySelector(".checkbox");
 
 // Load all event listeners
 loadEventListeners();
@@ -23,6 +23,8 @@ function loadEventListeners() {
     taskList.addEventListener('click', deleteTodo)
     clearBtn.addEventListener('click', clearTasks);
     filter.addEventListener('keydown', filterTask)
+    checkboxRow.addEventListener("click", cancelTodo);
+    checkboxRow.addEventListener("click", deleteCheckboxTodo);
 
     //Local Storage
     document.addEventListener("DOMContentLoaded", getTaskFromLocalStorage)
@@ -45,13 +47,16 @@ function createElements(task, taskDate){
 
     //create columns with class names
     const firstCol = document.createElement("div");
-    firstCol.className = "col s3";
+    firstCol.className = "col s2";
 
     const secondCol = document.createElement("div");
-    secondCol.className = "col s6 task-item";
+    secondCol.className = "col s5 task-item";
 
     const thirdCol = document.createElement("div");
     thirdCol.className = "col s3 time";
+
+    const fourthCol =  document.createElement('div');
+    fourthCol.className = "col s2 delete ";
 
     //create child elements for first column
     let checkboxContainer = document.createElement("p");
@@ -60,9 +65,9 @@ function createElements(task, taskDate){
 
     const label = document.createElement("label");
     const checkbox = document.createElement("input");
-    checkbox.className = "filled-in";
+    checkbox.className = "checkbox";
     checkbox.setAttribute("type", "checkbox")
-    checkbox.setAttribute("checked", "checked");
+    // checkbox.setAttribute("checked", "checked");
     const span = document.createElement("span");
     span.innerHTML = " ";
 
@@ -90,13 +95,21 @@ function createElements(task, taskDate){
     //append dateElement to third col
     thirdCol.appendChild(dateElement);
 
+
+    let deleteTag = document.createElement('a')
+    deleteTag.className = "delete-item";
+    deleteTag.innerHTML = '<i class="fa fa-remove"></i>';
+
+    fourthCol.appendChild(deleteTag);
+
     //append columns to task row
     taskRow.appendChild(firstCol);
     taskRow.appendChild(secondCol);
     taskRow.appendChild(thirdCol);
+    taskRow.appendChild(fourthCol);
 
     documentFragment.appendChild(taskRow);
-    card.appendChild(documentFragment);
+    checkboxRow.appendChild(documentFragment);
 }
 
 // Add Task
@@ -126,63 +139,64 @@ function addTask(e) {
 
 
             //Checkboxes implementation
-          const taskRow = document.createElement("div");
-          taskRow.className = "row task-row";
-
-          //create columns with class names
-          const firstCol = document.createElement("div");
-          firstCol.className = "col s3";
-
-          const secondCol = document.createElement("div");
-          secondCol.className = "col s6 task-item";
-
-          const thirdCol = document.createElement("div");
-          thirdCol.className = "col s3 time";
-
-          //create child elements for first column
-          let checkboxContainer = document.createElement("p");
-          checkboxContainer.className = "label-container"
-
-
-          const label = document.createElement("label");
-          const checkbox = document.createElement("input");
-          checkbox.className = "filled-in";
-          checkbox.setAttribute("type", "checkbox")
-          checkbox.setAttribute("checked", "checked");
-          const span = document.createElement("span");
-          span.innerHTML = " ";
-
-          label.appendChild(checkbox);
-          label.appendChild(span);
-
-          //append children of checkbox container to it
-          checkboxContainer.appendChild(label);
-
-          // append checkbox container to first col
-          firstCol.appendChild(checkboxContainer);
-
-          //create children of second column
-          const taskItem = document.createElement("p");
-          taskItem.className = "item";
-          taskItem.innerHTML = taskInput.value;
-
-          //append taskItem to second col
-          secondCol.appendChild(taskItem);
-
-          //create children of third col
-          const dateElement = document.createElement("p");
-          dateElement.className = "dateElement";
-          dateElement.innerText = moment(date.value).format("MMM DD");
-          //append dateElement to third col
-          thirdCol.appendChild(dateElement);
-
-          //append columns to task row
-          taskRow.appendChild(firstCol);
-          taskRow.appendChild(secondCol);
-          taskRow.appendChild(thirdCol);
-
-          documentFragment.appendChild(taskRow);
-          card.appendChild(documentFragment);
+          createElements(taskInput.value, date.value);
+          // const taskRow = document.createElement("div");
+          // taskRow.className = "row task-row";
+          //
+          // //create columns with class names
+          // const firstCol = document.createElement("div");
+          // firstCol.className = "col s3";
+          //
+          // const secondCol = document.createElement("div");
+          // secondCol.className = "col s6 task-item";
+          //
+          // const thirdCol = document.createElement("div");
+          // thirdCol.className = "col s3 time";
+          //
+          // //create child elements for first column
+          // let checkboxContainer = document.createElement("p");
+          // checkboxContainer.className = "label-container"
+          //
+          //
+          // const label = document.createElement("label");
+          // const checkbox = document.createElement("input");
+          // checkbox.className = "checkbox";
+          // checkbox.setAttribute("type", "checkbox")
+          // // checkbox.setAttribute("checked", "checked");
+          // const span = document.createElement("span");
+          // span.innerHTML = " ";
+          //
+          // label.appendChild(checkbox);
+          // label.appendChild(span);
+          //
+          // //append children of checkbox container to it
+          // checkboxContainer.appendChild(label);
+          //
+          // // append checkbox container to first col
+          // firstCol.appendChild(checkboxContainer);
+          //
+          // //create children of second column
+          // const taskItem = document.createElement("p");
+          // taskItem.className = "item";
+          // taskItem.innerHTML = taskInput.value;
+          //
+          // //append taskItem to second col
+          // secondCol.appendChild(taskItem);
+          //
+          // //create children of third col
+          // const dateElement = document.createElement("p");
+          // dateElement.className = "dateElement";
+          // dateElement.innerText = moment(date.value).format("MMM DD");
+          // //append dateElement to third col
+          // thirdCol.appendChild(dateElement);
+          //
+          // //append columns to task row
+          // taskRow.appendChild(firstCol);
+          // taskRow.appendChild(secondCol);
+          // taskRow.appendChild(thirdCol);
+          //
+          // documentFragment.appendChild(taskRow);
+          // checkboxRow.appendChild(documentFragment);
 
           saveTaskInLocalStorage(taskInput.value, date.value);
 
@@ -200,6 +214,123 @@ function deleteTodo(e) {
       }
     deleteTaskFromLocalStorage(e.target.parentElement.parentElement)
 
+}
+
+function deleteCheckboxTodo(e) {
+        console.log(e.target.parentElement)
+    if (event.target.parentElement.className === "delete-item") {
+        if (e.target.parentElement.classList.contains("delete-item")) {
+            console.log('it contains')
+            e.target.parentElement.parentElement.parentElement.remove();
+        }
+        deleteCheckBoxTaskFromLocalStorage(e.target.parentElement.parentElement.parentElement)
+
+    }
+
+    if (event.target.parentElement.firstChild.className === "checkbox") {
+        console.log("it is a checkbox")
+    }
+
+}
+
+function cancelTodo(e) {
+    const allCheckoxes = document.querySelectorAll('.task-row')
+    console.log(allCheckoxes)
+    // let myElementClone = e.target.parentElement.parentElement.parentElement.parentElement.cloneNode(true);
+
+    if (e.target.checked === true) {
+        // let myElementClone;
+
+
+    //     if (!myElementClone) {
+    //         console.log("no clone")
+    //         myElementClone = e.target.parentElement.parentElement.parentElement.parentElement.cloneNode(true);
+    //         e.target.parentElement.parentElement.parentElement.parentElement.style.display = "none";
+    //         myElementClone.firstChild.nextSibling.firstChild.style.textDecoration = "line-through";
+    //         myElementClone.style.display = "block";
+    //         checkboxRow.appendChild(myElementClone)
+    //
+    //     } else if (myElementClone) {
+    //         console.log('there is a clone')
+    //         myElementClone = e.target.parentElement.parentElement.parentElement.parentElement.cloneNode(true);
+    //
+    //         myElementClone.style.display = "block";
+    //
+    //     }
+    // }
+
+
+        allCheckoxes.forEach((item, i) => {
+         let myElementClone = e.target.parentElement.parentElement.parentElement.parentElement.cloneNode(true);
+            // console.log(item.firstChild.nextSibling.firstChild.innerHTML)
+            if(myElementClone.firstChild.nextSibling.firstChild.innerHTML === item.firstChild.nextSibling.firstChild.innerHTML) {
+               console.log("equal")
+                e.target.parentElement.parentElement.parentElement.parentElement.style.display = "none";
+                myElementClone.firstChild.nextSibling.firstChild.style.textDecoration = "line-through";
+                myElementClone.style.display = "block";
+                console.log(checkboxRow.lastChild.firstChild.nextSibling.firstChild.innerHTML);
+                if (checkboxRow.lastChild.firstChild.nextSibling.firstChild.innerHTML !== myElementClone.firstChild.nextSibling.firstChild.innerHTML) {
+                    checkboxRow.appendChild(myElementClone)
+
+                } else if (checkboxRow.lastChild.firstChild.nextSibling.firstChild.innerHTML ===  myElementClone.firstChild.nextSibling.firstChild.innerHTML) {
+                    checkboxRow.appendChild(myElementClone)
+
+                }
+                // item.style.display = "block";
+                // console.log(item.firstChild.firstChild.firstChild.firstChild.checked)
+                // item.firstChild.firstChild.firstChild.firstChild.checked = false;
+                // item.firstChild.nextSibling.firstChild.style.textDecoration = "none"
+                // e.target.parentElement.parentElement.parentElement.parentElement.style.display = "none";
+
+            }else {
+                // e.target.parentElement.parentElement.parentElement.parentElement.style.display = "none";
+                // myElementClone.firstChild.nextSibling.firstChild.style.textDecoration = "line-through";
+                // checkboxRow.appendChild(myElementClone)
+            }
+        })
+        // e.target.parentElement.parentElement.parentElement.nextSibling.firstChild.style.textDecoration = "line-through";
+        // console.log(e.target.parentElement.parentElement.parentElement.parentElement)
+
+    }
+    if (e.target.checked === false) {
+        for (let i = 0; i < allCheckoxes.length; i++) {
+            if(e.target.parentElement.parentElement.parentElement.nextSibling.firstChild.innerHTML === allCheckoxes[i].firstChild.nextSibling.firstChild.innerHTML) {
+                console.log('it is equal');
+                allCheckoxes[i].style.display = "block";
+                allCheckoxes[i].firstChild.firstChild.firstChild.firstChild.checked = false;
+                // item.firstChild.nextSibling.firstChild.style.textDecoration = "none"
+                // e.target.parentElement.parentElement.parentElement.parentElement.style.display = "none";
+                console.log(allCheckoxes[i].parentNode);
+                console.log(e.target.parentElement.parentElement.parentElement.parentElement);
+                e.target.parentElement.parentElement.parentElement.parentElement.parentNode.removeChild( e.target.parentElement.parentElement.parentElement.parentElement);
+                break;
+            }
+        }
+        console.log(allCheckoxes);
+
+
+        // allCheckoxes.forEach((item, i) => {
+        //     console.log(item)
+        //     // console.log('inner html', e.target.parentElement.parentElement.parentElement.nextSibling.firstChild.innerHTML)
+        //     if(e.target.parentElement.parentElement.parentElement.nextSibling.firstChild.innerHTML === item.firstChild.nextSibling.firstChild.innerHTML) {
+        //         console.log('it is equal');
+        //         item.style.display = "block";
+        //         item.firstChild.firstChild.firstChild.firstChild.checked = false;
+        //         // item.firstChild.nextSibling.firstChild.style.textDecoration = "none"
+        //         // e.target.parentElement.parentElement.parentElement.parentElement.style.display = "none";
+        //         console.log(item.parentNode);
+        //         console.log(e.target.parentElement.parentElement.parentElement.parentElement);
+        //         // e.target.parentElement.parentElement.parentElement.parentElement.parentNode.removeChild( e.target.parentElement.parentElement.parentElement.parentElement);
+        //
+        //     }
+
+
+        // })
+
+        e.target.parentElement.parentElement.parentElement.nextSibling.firstChild.style.textDecoration = "none";
+
+        // e.target.parentElement.parentElement.parentElement.parentElement.style.display = "block";
+    }
 
 }
 
@@ -251,7 +382,7 @@ function getTaskFromLocalStorage() {
 
       //loop through task array to display available todo items
       tasks.forEach((task) => {
-          console.log(task)
+          // console.log(task)
               // Create li element
               const li = document.createElement('li');
               // Add class
@@ -276,6 +407,8 @@ function getTaskFromLocalStorage() {
 
 //delete a task from Local Storage
 function deleteTaskFromLocalStorage(deletedNodeElement) {
+    console.log(deletedNodeElement);
+    console.log(deletedNodeElement.textContent)
     let tasks;
 
     if (localStorage.getItem("tasks") === null) {
@@ -287,7 +420,31 @@ function deleteTaskFromLocalStorage(deletedNodeElement) {
     //loop through tasks array and deleted the item that matches the text
     //content of the deleted node element
     tasks.forEach((item, index) => {
+        console.log(item)
         if (deletedNodeElement.textContent === item) {
+            tasks.splice(index, 1)
+        }
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}
+
+//delete checkbox task from Local Storage
+function deleteCheckBoxTaskFromLocalStorage(deletedNodeElement) {
+    console.log(deletedNodeElement);
+    let tasks;
+
+    if (localStorage.getItem("tasks") === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem("tasks"))
+    }
+
+    //loop through tasks array and deleted the item that matches the text
+    //content of the deleted node element
+    tasks.forEach((item, index) => {
+        console.log(item)
+        if (deletedNodeElement.firstChild.nextSibling.firstChild.textContent === item.task) {
             tasks.splice(index, 1)
         }
     });
